249c533edb3de3a082796d7abc64e91eb87d0b9a
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-07-04 20:24:58 +0700

    [RIZAL][MODIFY] db production

--- src/config/config.js
+++ src/config/config.js
@@ -1,23 +1,27 @@
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": "Rizalmohamad123",
-    "database": "ektsp",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    username: "root",
+    password: "Rizalmohamad123",
+    database: "ektsp",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgress",
+    protocol: "postgress",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    }
   }
 }
