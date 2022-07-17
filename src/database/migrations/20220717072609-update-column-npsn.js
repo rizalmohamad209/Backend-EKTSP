9218dc5ca01191572dce9438141b280c61488f98
rizalmohamad209 <rizalmohamad209@gmail.com>
2022-07-17 14:27:55 +0700

    [RIZAL][ADD] - add migrate update column

---
+++ src/database/migrations/20220717072609-update-column-npsn.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('sekolahs', 'npsn', {
+      type: Sequelize.BIGINT,
+      allowNull: true,
+    });
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('sekolahs');
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
